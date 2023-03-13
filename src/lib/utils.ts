5f276119566e649e2eb4397561eb6f03e1c3055b
Giulio Pezzulli <giulio@aitutor.co.uk>
2023-03-13 15:30:07 +0000

    fix url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const getUtorgUrl = (address: string, timestamp: string, signature: string) => {
 	const UTORG_SID = process.env.NEXT_PUBLIC_UTORG_SID
 	const urlBase = process.env.NODE_ENV === 'production' ? 'app' : 'app-stage'
-	const url = `https://${urlBase}.utorg.pro/direct/${UTORG_SID}?&currency=CMP&timestamp=${timestamp}&alg=WEB3&publicKey=${address}&signature=${signature}`
+	const url = `https://${urlBase}.utorg.pro/direct/${UTORG_SID}/${address}?&currency=CMP&timestamp=${timestamp}&alg=WEB3&publicKey=${address}&signature=${signature}`
 	console.log('UTORG', url)
 	return url
 }
